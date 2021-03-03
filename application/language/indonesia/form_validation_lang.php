a1de720bcd66265004685525cc027f611611754c
Usman Juhari <kangusman1987@gmail.com>
2021-03-03 11:50:51 +0700

    add indonesia form validation language

---
+++ application/language/indonesia/form_validation_lang.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required'] = '{field} wajib diisi!';
+$lang['form_validation_isset'] = '{field} harus memiliki nilai!';
+$lang['form_validation_valid_email'] = '{field} harus berisi alamat email yang valid!';
+$lang['form_validation_valid_emails'] = '{field} harus berisi semua alamat email yang valid!';
+$lang['form_validation_valid_url'] = '{field} harus berisi URL yang valid!';
+$lang['form_validation_valid_ip'] = '{field} harus berisi IP yang valid!';
+$lang['form_validation_min_length'] = '{field} harus memiliki setidaknya {param} karakter!';
+$lang['form_validation_max_length'] = '{field} tidak dapat melebihi panjang {param} karakter!';
+$lang['form_validation_exact_length'] = '{field} harus tepat {param} karakter!';
+$lang['form_validation_alpha'] = '{field} hanya boleh berisi karakter huruf!';
+$lang['form_validation_alpha_numeric'] = '{field} hanya boleh berisi karakter nomor dan huruf!';
+$lang['form_validation_alpha_numeric_spaces'] = '{field} hanya boleh berisi karakter nomor, huruf dan spasi!';
+$lang['form_validation_alpha_dash'] = '{field} hanya boleh berisi karakter huruf dan angka, garis bawah (_) dan tanda pisah (-)!';
+$lang['form_validation_numeric'] = '{field} harus diisi angka!';
+$lang['form_validation_is_numeric'] = '{field} hanya diisi karakter numerik!';
+$lang['form_validation_integer'] = '{field} hanya diisi integer!';
+$lang['form_validation_regex_match'] = '{field} format harus benar!';
+$lang['form_validation_matches'] = '{field} tidak cocok dengan kolom {param}!';
+$lang['form_validation_differs'] = '{field} harus beda dengan kolom {param}!';
+$lang['form_validation_is_unique'] = '{field} harus diisi dengan nilai unik!';
+$lang['form_validation_is_natural'] = '{field} hanya boleh diisi angka!';
+$lang['form_validation_is_natural_no_zero'] = '{field} hanya boleh diisi angka dan harus lebih dari nol!';
+$lang['form_validation_decimal'] = '{field} harus diisi angka desimal!';
+$lang['form_validation_less_than'] = '{field} field must contain a number less than {param}.';
+$lang['form_validation_less_than_equal_to'] = '{field} harus diisi angka yang kurang dari atau sama dengan {param}.';
+$lang['form_validation_greater_than'] = '{field} harus diisi angka yang lebih besar dari {param}.';
+$lang['form_validation_greater_than_equal_to'] = '{field} harus berisi angka yang lebih besar atau sama dengan {param}.';
+$lang['form_validation_error_message_not_set'] = 'Tidak dapat mengakses pesan kesalahan yang sesuai dengan {field}.';
+$lang['form_validation_in_list'] = '{field} harus salah satu dari {param}!';
